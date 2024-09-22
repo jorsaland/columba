520cf1665e6ee0cdcdc55e8e79a94b11414f105b
jorsaland <andresaldarriaga.94@gmail.com>
2024-09-22 11:08:03 -0500

    better formatted code in function convert_time

--- app/utils/convert_time/_convert_time.py
+++ app/utils/convert_time/_convert_time.py
@@ -15,6 +15,8 @@ def convert_time(event_time: str):
 
     pattern_1 = r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$'
     pattern_2 = r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$'
+
     if not (re.match(pattern_1, event_time) or re.match(pattern_2, event_time)):
         raise ValueError('time format must be: YYYY-MM-DD hh:mm')
+
     return datetime.fromisoformat(event_time)
